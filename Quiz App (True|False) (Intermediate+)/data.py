8268bc7a21eff295e9fcb86724b84a060f00e99a
Saurav Lakhmani <71827469+SLakhmani@users.noreply.github.com>
2022-06-16 15:12:04 +0100

    Create data.py

---
+++ Quiz App (True|False) (Intermediate+)/data.py
@@ -0,0 +1,9 @@
+import requests
+
+parameters = {
+    "amount": 20,
+    "type": "boolean",
+}
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+question_data = response.json()["results"]
