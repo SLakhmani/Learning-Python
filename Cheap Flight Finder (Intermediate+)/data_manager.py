cff0dde989e986b7c8307797955aa54c7217f5eb
Saurav Lakhmani <71827469+SLakhmani@users.noreply.github.com>
2022-06-24 14:52:15 +0100

    Update data_manager.py

--- Cheap Flight Finder (Intermediate+)/data_manager.py
+++ Cheap Flight Finder (Intermediate+)/data_manager.py
@@ -6,6 +6,7 @@ class DataManager:
     # This class is responsible for talking to the Google Sheet.
     def __init__(self):
         self.API_ENDPOINT = os.environ.get("SHEETY_ENDPOINT")
+        self.EMAILS_ENDPOINT = os.environ.get("EMAILS_ENDPOINT")
 
     def get_sheet_data(self):
         response = requests.get(self.API_ENDPOINT)
@@ -20,3 +21,11 @@ class DataManager:
 
         response = requests.put(row_endpoint, json=row_data)
         response.raise_for_status()
+
+    def get_emails(self):
+        response = requests.get(self.EMAILS_ENDPOINT)
+        user_data = response.json()["users"]
+        email_list = []
+        for users in user_data:
+            email_list.append((users["email"], users["firstName"]))
+        return email_list
